bugfix: expressions like 'np.array([1,2,3]) * units.meter' should produce a quantity-wrapped array, not an array of quantities

smoot/unit.py
@@ -21,6 +21,11 @@ OperatorUnitLike = Union["Unit", float, int, Iterable[float], Iterable[int]]
 class Unit:
     __slots__ = ("__inner", "__registry")
 
+    # Make unit multiplication higher priority than numpy array multiplication.
+    # This is needed to ensure that expressions like `np.array([1, 2, 3]) * units.meter`
+    # result in a quantity-wrapped array rather than an array of quantities.
+    __array_priority__ = 10
+
     def __init__(self) -> None:
         raise NotImplementedError
 
@@ -128,9 +133,12 @@ class Unit:
             new = object.__new__(self.__registry.Quantity)
             arr = np.array(other, dtype=np.float64)
             new._Quantity__inner = arr_mul_unit(arr=arr, unit=self.__inner)
-        else:
+        elif isinstance(other, Unit):
             new = object.__new__(self.__class__)
             new.__inner = self.__inner * other.__inner
+        else:
+            msg = f"Type {type(other)} cannot multiply a unit"
+            raise NotImplementedError(msg)
         return new
 
     def __rmul__(self, other: OperatorUnitLike) -> Unit | smoot.Quantity:

tests/test_registry.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import typing
 
+import numpy as np
 import pytest
 from smoot import UnitRegistry, Unit, SmootError
 import smoot
@@ -103,9 +104,11 @@ def test_units_multiply_into_quantity(units: UnitRegistry) -> None:
     assert (units.meter * 1) == Q("1 meter")
     assert (1 * units.meter) == Q("1 meter")
 
-    # array
+    # array, multiplication is associative
     assert (([1, 2] * units.meter) == Q([1, 2], units.meter)).all()
     assert ((units.meter * [1, 2]) == Q([1, 2], units.meter)).all()
+    assert (np.array([1, 2, 3]) * units.meter == Q([1, 2, 3], units.meter)).all()
+    assert (units.meter * np.array([1, 2, 3]) == Q([1, 2, 3], units.meter)).all()
 
 
 def test_units_divide(units: UnitRegistry) -> None: